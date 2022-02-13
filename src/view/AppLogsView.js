32ce9848e0704ab1bc1744100e5d119229ba3bf9
mike wakerly <opensource@hoho.com>
2022-02-13 13:38:26 -0500

    logs: press to show details, long press to copy message

--- src/view/AppLogsView.js
+++ src/view/AppLogsView.js
@@ -1,10 +1,13 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 
+import * as Clipboard from 'expo-clipboard';
 import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Title } from 'react-native-paper';
 
 import ApiContext from '../component/ApiContext';
 import CurrentAppContext from '../component/CurrentAppContext';
+import { LongDateAndTime } from '../component/TimeUtil';
 import GlobalStyles from '../lib/GlobalStyles';
 import { useTimeout } from '../lib/Hooks';
 import { getLogger } from '../lib/Logging';
@@ -14,6 +17,32 @@ const POLL_INTERVAL_MILLIS = 5000;
 
 const debug = getLogger('AppLogsView');
 
+const LogEntry = ({ log, isExpanded = false, style }) => {
+  const logStyles = isExpanded ? [styles.expandedLog, style] : [style];
+  return (
+    <View style={logStyles} key={log.id}>
+      <Text style={GlobalStyles.logText}>{log.attributes.message}</Text>
+      {isExpanded ? (
+        <View style={{ flexDirection: 'row' }}>
+          <Text
+            style={{
+              marginRight: 4,
+              textTransform: 'uppercase',
+              backgroundColor: '#888',
+              borderRadius: 3,
+              paddingLeft: 3,
+              paddingRight: 3,
+            }}
+          >
+            {log.attributes.level}
+          </Text>
+          <LongDateAndTime style={styles.logDateTime} datetime={log.attributes.timestamp} />
+        </View>
+      ) : null}
+    </View>
+  );
+};
+
 const AppLogsView = ({ route, navigation }) => {
   const { app } = useContext(CurrentAppContext);
   const [logs, setLogs] = useState([]);
@@ -21,6 +50,7 @@ const AppLogsView = ({ route, navigation }) => {
   const [refreshing, setRefreshing] = React.useState(true);
   const { apiClient } = useContext(ApiContext);
   const [nextTimeout, setNextTimeout] = useState(null);
+  const [expandedLogId, setExpandedLogId] = useState(null);
 
   const logKeys = new Set(logs.map((l) => l.attributes.id));
 
@@ -60,12 +90,29 @@ const AppLogsView = ({ route, navigation }) => {
     setNextTimeout(0);
   }, []);
 
+  const onLogEntryPress = (log) => {
+    if (log.id === expandedLogId) {
+      setExpandedLogId(null);
+    } else {
+      setExpandedLogId(log.id);
+    }
+  };
+
+  const onLogEntryLongPress = (log) => {
+    // TODO(mikey): Show a "log copied!" toast
+    Clipboard.setString(log.attributes.message);
+  };
+
   const logItems = logs.map((log, idx) => {
     const style = idx % 2 ? styles.logLineEven : styles.logLineOdd;
+    const isExpanded = log.id === expandedLogId;
     return (
-      <View style={style} key={log.id}>
-        <Text style={GlobalStyles.logText}>{log.attributes.message}</Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => onLogEntryPress(log)}
+        onLongPress={() => onLogEntryLongPress(log)}
+      >
+        <LogEntry style={style} log={log} key={log.id} isExpanded={isExpanded} />
+      </TouchableOpacity>
     );
   });
   logItems.reverse();
@@ -100,6 +147,12 @@ const styles = StyleSheet.create({
   logLineOdd: {
     backgroundColor: '#00000011',
     padding: 6,
+  },
+  logDateTime: {
+    fontWeight: 'bold',
+  },
+  expandedLog: {
+    backgroundColor: 'lightgreen',
   },
 });
 
