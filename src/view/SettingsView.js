13f863badc9f35e3167a5f8b1aa43fb6438b2d29
mike wakerly <opensource@hoho.com>
2022-02-13 13:45:26 -0500

    settings: delete env switcher, not currently useful

--- src/view/SettingsView.js
+++ src/view/SettingsView.js
@@ -2,41 +2,16 @@ import { useContext } from 'react';
 
 import Constants from 'expo-constants';
 import { Linking, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Button, Card, RadioButton } from 'react-native-paper';
+import { Button, Card } from 'react-native-paper';
 
-import EnvironmentContext, { ENVIRONMENTS } from '../component/EnvironmentContext';
 import SettingsContext from '../component/SettingsContext';
 
 export default SettingsView = () => {
   const { resetInstallation } = useContext(SettingsContext);
 
-  const { environment, isExpoGo, setEnvironment } = useContext(EnvironmentContext);
-
   const version = Constants.manifest.version;
   const releaseId = Constants.manifest.extra?.expoClient?.releaseId || '';
   const revisionId = Constants.manifest.extra?.expoClient?.revisionId || '';
-
-  const getEnvSwitcher = () => {
-    if (!isExpoGo) {
-      return null;
-    }
-    const buttons = Object.entries(ENVIRONMENTS).map(([envName]) => {
-      return (
-        <View key={envName} style={styles.radioContainer}>
-          <RadioButton key={envName} value={envName} />
-          <Text>{envName}</Text>
-        </View>
-      );
-    });
-    return (
-      <RadioButton.Group
-        onValueChange={(newValue) => setEnvironment(newValue)}
-        value={environment.name}
-      >
-        {buttons}
-      </RadioButton.Group>
-    );
-  };
 
   return (
     <View style={styles.container}>
@@ -49,7 +24,6 @@ export default SettingsView = () => {
               {releaseId ? `release ${releaseId}` : null}{' '}
               {revisionId ? `revision ${revisionId}` : null}
             </Text>
-            {getEnvSwitcher()}
             <Button
               style={{ marginTop: 20 }}
               icon="globe"
@@ -79,14 +53,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     width: '100%',
-  },
-  radioContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-    paddingStart: 8,
-    paddingEnd: 8,
-    borderRadius: 4,
   },
   scrollView: {
     width: '100%',
